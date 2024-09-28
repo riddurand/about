c3722f0599ce5602e2f3566e2144d9e23f616f55
riddurand <riddumusic@gmail.com>
2024-09-28 18:52:37 +0600

    Add files via upload

---
+++ js/bootstap.min.js
@@ -0,0 +1,8 @@
+const myTooltipEl = document.getElementById('myTooltip')
+const tooltip = bootstrap.Tooltip.getOrCreateInstance(myTooltipEl)
+
+myTooltipEl.addEventListener('hidden.bs.tooltip', () => {
+  // do something...
+})
+
+tooltip.hide()
